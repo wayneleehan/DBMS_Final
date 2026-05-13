a27d30b030a0dcb3f4a02d37185bb54b5ddf3cae
陳致安 <183062837+nccu1133060@users.noreply.github.com>
2026-05-13 14:19:22 +0800

    Revert "Scrum 29 website early warning"

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,12 +1,2 @@
-from sqlalchemy.orm import Session
 from sqlalchemy import text
-
-def update_reliability_score(db: Session, user_id: int, delta: float):
-    
-    sql = text("""
-        UPDATE users 
-        SET Reliability_Score = LEAST(100, GREATEST(0, Reliability_Score + :delta))
-        WHERE User_ID = :user_id
-    """)
-    db.execute(sql, {"delta": delta, "user_id": user_id})
-    db.commit()
+from sqlalchemy.orm import Session
